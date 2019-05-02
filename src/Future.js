c1e8886573483a8c870623f92fe5c684ebd465af
Maurice Domínguez <maurice.ronet.dominguez@gmail.com>
2019-05-02 11:24:49 +0200

    feature: adds Future

---
+++ src/Future.js
@@ -0,0 +1,64 @@
+const {
+  inspect
+} = require('util')
+
+const {
+  construct
+} = require('./util')
+
+const {
+  pipe
+} = require('ramda')
+
+class Future {
+  constructor(fork){
+		this.fork = fork
+	}
+
+	// -- pointed
+	static of(value){
+		return new Future((reject, resolve) => resolve(value))
+	}
+
+	// -- functor
+	map(f){
+		return new Future((reject, resolve) => {
+			this.fork(reject, pipe(f, resolve))
+		})
+	}
+
+	// -- monad
+	chain(f){
+		return this.map(f).join()
+	}
+
+	join(){
+		return new Future((reject, resolve) => {
+			return this.fork(reject, (future) => future.fork(reject, resolve))
+		})
+	}
+
+	// -- applicative
+	ap(future){
+		return this.map(f => future.map(f)).join()
+	}
+  // -- utils
+
+  toString() {
+    return `Future(?)`
+  }
+
+  [inspect.custom]() {
+    return this.toString()
+	}
+	
+	// futurify :: nodeCallback -> * -> Future e b 
+	static futurify(f){
+		return (...args) => {
+			return new Future((reject, resolve) => 
+				f(...args, (err, data) => err ? reject(err) : resolve(data)))
+		}
+	}
+}
+
+module.exports = construct(Future)
